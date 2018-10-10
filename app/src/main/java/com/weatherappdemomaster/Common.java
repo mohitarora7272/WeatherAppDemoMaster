3019dd0fe3746701097efd5b1c317e33085b9a93
mohitarora7272 <mohitarora7272@gmail.com>
2018-10-10 17:05:58 +0530

    Initial commit

---
+++ app/src/main/java/com/weatherappdemomaster/Common.java
@@ -0,0 +1,9 @@
+package com.weatherappdemomaster;
+
+public class Common {
+
+    public static final String OPEN_WEATHER_MAP_API = "https://api.openweathermap.org/data/2.5/forecast?q=%s&units=metric";
+    public static final String OPEN_WEATHER_MAP_API2 = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric";
+    public static final String OPEN_WEATHER_MAP_API3 = "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+    public static final String ROOT_IMAGE = "http://openweathermap.org/img/w/";
+}
